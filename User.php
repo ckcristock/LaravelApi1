6b5faa81aa60aa69f9ac29f3f9650803ed93f493
Cristian Montoya <86530903+ckcristock@users.noreply.github.com>
2024-07-31 08:54:23 -0500

    Create User.php

---
+++ User.php
@@ -0,0 +1,19 @@
+<?PHP
+
+namespace app\models;
+use illuminate\database\ elocuent\factories\hasFactory;
+use illuminate\foundation\Auth\user as authenticatable;
+use illuminate\notifications\notificable;
+use laravel\sanctum\HasApiTokens;
+
+class User extends Authenticatetable
+{
+use HasApiTokens, HasFactory, notifiable;
+@var array<int>
+protected $fillable = [
+'name',
+'email',
+'password',
+];
+
+}
